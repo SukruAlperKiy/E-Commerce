28cd95409d0d240efc053c8ca24bdad2cc344f2d
burgerkingvscode <burgerkingvscode@gmail.com>
2026-05-31 20:13:51 +0300

    sldier

---
+++ PresentationLayer/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PresentationLayer.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Default()
+        {
+            return View();
+        }
+    }
+}
