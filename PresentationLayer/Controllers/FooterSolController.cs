55808e8367be944678b975b1ec4c2401c4272551
burgerkingvscode <burgerkingvscode@gmail.com>
2026-06-03 14:53:54 +0300

    footersol finished

--- PresentationLayer/Controllers/FooterSolController.cs
+++ PresentationLayer/Controllers/FooterSolController.cs
@@ -1,4 +1,5 @@
 ﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace PresentationLayer.Controllers
@@ -12,7 +13,40 @@ namespace PresentationLayer.Controllers
         }
         public IActionResult FooterSolList()
         {
+            var degerler = _footerSolService.IFooterSolServiceGetAll();
+            return View(degerler);
+        }
+
+        [HttpGet]
+        public IActionResult CreateSolFooter()
+        {
             return View();
         }
+        [HttpPost]
+        public IActionResult CreateSolFooter(FooterSol varlik)
+        {
+            _footerSolService.IFooterSolServiceInsert(varlik);
+            return RedirectToAction("FooterSolList");
+        }
+        [HttpGet]
+        public IActionResult UpdateSolFooter(int id)
+        {
+            var deger = _footerSolService.IFooterSolServiceGetById(id);
+            return View(deger);
+        }
+        [HttpPost]
+        public IActionResult UpdateSolFooter(FooterSol varlik)
+        {
+            _footerSolService.IFooterSolServiceUpdate(varlik);
+            return RedirectToAction("FooterSolList");
+        }
+
+        public IActionResult DeleteSolFooter(int id)
+        {
+            _footerSolService.IFooterSolServiceDelete(id);
+            return RedirectToAction("FooterSolList");
+        }
+
+            
     }
 }
