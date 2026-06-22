b471436f08b43fdcce8f9c719cb42624463f0805
burgerkingvscode <burgerkingvscode@gmail.com>
2026-06-22 19:27:15 +0300

    header css changes

--- PresentationLayer/ViewComponents/HomeViewComponents/_HomeHeaderAreaSectionComponentPartial.cs
+++ PresentationLayer/ViewComponents/HomeViewComponents/_HomeHeaderAreaSectionComponentPartial.cs
@@ -1,10 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PresentationLayer.Models;
 namespace PresentationLayer.ViewComponents.HomeViewComponents
 {
     public class _HomeHeaderAreaSectionComponentPartial:ViewComponent
     {
+        private readonly dal _dal;
+        public _HomeHeaderAreaSectionComponentPartial(dal dal123)
+        {
+            _dal = dal123;
+        }
         public IViewComponentResult Invoke()
         {
+            string kategorilerQuery = "Select kategoriIsim, kategoriStatus from Kategoriler";
+            ViewBag.kategorilerViewBag = _dal.CommandExecuteReader(kategorilerQuery, _dal.benimSqlBaglantim);
+
             return View();
         }
     }
--- PresentationLayer/ViewComponents/HomeViewComponents/_HomeNavbarSectionComponentPartial.cs
+++ PresentationLayer/ViewComponents/HomeViewComponents/_HomeNavbarSectionComponentPartial.cs
@@ -1,10 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+using PresentationLayer.Models;
+using System.Data;
 namespace PresentationLayer.ViewComponents.HomeViewComponents
 {
     public class _HomeNavbarSectionComponentPartial:ViewComponent 
     {
+        private readonly dal _dal;
+        public _HomeNavbarSectionComponentPartial(dal dal123)
+        {
+            _dal = dal123;
+        }
+
         public IViewComponentResult Invoke()
         {
+            string navbarKategorileriQuery = "Select kategoriIsim, kategoriStatus from Kategoriler";
+            ViewBag.verilerViewBag = _dal.CommandExecuteReader(navbarKategorileriQuery, _dal.benimSqlBaglantim);
+
             return View();
         }
     }
